1086c59ccfc97b54a954c47c9d3eb1510031c095
MylesGem <sasquatchgemignani@gmail.com>
2023-06-04 21:28:23 -0700

    still massive errors but i dont know where

--- miniature-eureka-main/Develop/server.js
+++ miniature-eureka-main/Develop/server.js
@@ -1,22 +1,27 @@
 // Dependencies
 const express = require("express");
+const cors = require("cors");
 const fs = require("fs");
-
+const path = require("path");
 
 // Sets up the express app
 var app = express();
-var PORT = process.env.PORT || 3001
+var PORT = process.env.PORT || 3001;
+
+// Enable CORS
+app.use(cors());
 
 // Sets up the express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/public/assets", express.static(__dirname + "/public/assets"));
 
+// Serve static files
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 require("./routes/html-routes")(app);
 require("./routes/api-routes")(app);
 
 // Starts the server to begin listening
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+console.log("App listening on PORT " + PORT);
 });
